7fadfb805d9ddaf965de29e1f42c3ed3e2c9d5ea
Fancy <fancyboynet@gmail.com>
2019-08-09 17:29:44 +0800

    revert condition

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (Command, options) => {
   class SubCommand extends Command {
     async run () {
-      if (process.env.npm_execpath.indexOf('yarn') !== -1) {
+      if (process.env.npm_execpath.indexOf('yarn') === -1) {
         console.log('Please use yarn.')
         process.exit(1)
       }
